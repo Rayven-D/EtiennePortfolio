73a0da6ab1feb0681dbd14342a9b231081fcd0f4
Rayven <rjderay@gmail.com>
2026-03-23 23:20:53 -0500

    home apge

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', loadComponent: () => import('./components/home/home').then(m => m.Home)},
+    {path: 'projects', loadComponent: () => import('./components/projects/projects').then(m => m.Projects)},
+    {path: 'about', loadComponent: () => import('./components/about/about').then(m => m.About)},
+    {path: "**", redirectTo: '', pathMatch: 'full'}
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,5 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +9,13 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('etienne-portfolio');
+
+  constructor(
+    private _router: Router
+  ) {}
+
+  public navigateTo(path: string): void {
+    console.log('here')
+    this._router.navigate([path]);
+  }
 }
