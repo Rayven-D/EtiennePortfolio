ca2edc009371dd5001940c5ecdeb8c9afec47551
Rayven <rjderay@gmail.com>
2026-03-24 00:42:43 -0500

    Auto-generated commit

---
+++ chunk-NXF55UWH.js
@@ -0,0 +1,1 @@
+import{Ba as n,Ca as s,Fa as c,ra as o}from"./chunk-2XKW5TCM.js";var r=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=o({type:t,selectors:[["app-projects"]],decls:2,vars:0,template:function(e,a){e&1&&(n(0,"p"),c(1,"projects works!"),s())},encapsulation:2})};export{r as Projects};
