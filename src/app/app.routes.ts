a6e347ef7cf997f09f3bc1f428eddf6c5f303266
Rayven <rjderay@gmail.com>
2026-03-24 00:37:39 -0500

    about page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {path: '', loadComponent: () => import('./components/home/home').then(m => m.Home)},
+    {path: 'home', loadComponent: () => import('./components/home/home').then(m => m.Home)},
     {path: 'projects', loadComponent: () => import('./components/projects/projects').then(m => m.Projects)},
     {path: 'about', loadComponent: () => import('./components/about/about').then(m => m.About)},
-    {path: "**", redirectTo: '', pathMatch: 'full'}
+    {path: "**", redirectTo: 'home', pathMatch: 'full'}
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,18 +1,33 @@
+import { NgClass } from '@angular/common';
 import { Component, signal } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router, RouterOutlet, RouterLinkWithHref, RouterLinkActive } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLinkWithHref, RouterLinkActive, NgClass],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('etienne-portfolio');
-
+  public currentPage = 0;
   constructor(
-    private _router: Router
-  ) {}
+    private _router: Router,
+    private _activatedRoute: ActivatedRoute
+  ) {
+    this._router.events.pipe(filter(e => e instanceof NavigationEnd)).subscribe((e: NavigationEnd) => {
+      if(e.url === "home") {
+        this.currentPage = 0;
+      } else if(e.url === "/projects") {
+        this.currentPage = 1;
+      } else if(e.url === "/about") {
+        this.currentPage = 2;
+      }else{
+        this.currentPage = 0;
+      }
+    });
+  }
 
   public navigateTo(path: string): void {
     console.log('here')
