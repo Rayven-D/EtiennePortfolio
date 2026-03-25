934d05ac8b9c70fc36fb2b923e32aa62bb9471d0
Rayven <rjderay@gmail.com>
2026-03-24 21:03:12 -0500

    about me page update witha actual data

--- src/app/components/about/about.ts
+++ src/app/components/about/about.ts
@@ -9,10 +9,11 @@ import { Component } from '@angular/core';
 export class About {
 
   public skills : {skillName: string, level: number}[] = [
-    {skillName: 'SKILL_1', level: 67},
-    {skillName: 'SKILL_2', level: 69},
-    {skillName: 'SKILL_3', level: 40},
-    {skillName: 'SKILL_4', level: 80},
-    
+    {skillName: 'Create Problem-Solving', level: 90},
+    {skillName: 'Feedback-Driven', level: 90},
+    {skillName: 'Visual & UX Foundations', level: 85},
+    {skillName: 'Interaction & Prototyping', level: 85},
+    {skillName: 'Information Architecture', level: 85},
+    {skillName: 'Usability Focus', level: 75}
   ];
 }
