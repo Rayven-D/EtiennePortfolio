47d35dba9f04f3b04a9035aa144ea050cf4c0c01
Rayven <rjderay@gmail.com>
2026-04-07 21:22:36 -0500

    verbage and img changes

--- src/app/components/project-details/financial-management/financial-management.ts
+++ src/app/components/project-details/financial-management/financial-management.ts
@@ -24,4 +24,8 @@ export class FinancialManagement {
   public navigateToProjects(): void {
     this._router.navigate(['projects']);
   }
+
+  public navigateToFigma(): void {
+    window.open('https://www.figma.com/proto/rbssrVfTqvBhvfLBrTsOYC/UI2-Project-6--Wireframes?node-id=110-187&t=mdAALpMKUvRlKlIC-1&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=110%3A187&show-proto-sidebar=1', '_blank');
+  }
 }
